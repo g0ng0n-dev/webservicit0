030a2dc3ecf61ebd8e95b9fac20f4e8e42f6450e
Gon Gis <g0ng0n.dev@gmail.com>
2020-11-21 19:03:50 +0100

    adding code implementation to handle multipart files

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/g0ng0n-dev/webservicito/database"
 	"github.com/g0ng0n-dev/webservicito/product"
+	"github.com/g0ng0n-dev/webservicito/receipt"
 	_ "github.com/go-sql-driver/mysql"
 	"net/http"
 )
@@ -10,6 +11,7 @@ import (
 const apiBasePath = "/api"
 func main() {
 	database.SetupDatabase()
+	receipt.SetupRoute(apiBasePath)
 	product.SetupRoute(apiBasePath)
 	http.ListenAndServe(":5000", nil)
 }
