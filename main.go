fd501f73b5344b809ca6e1f2dbad54eb4b9933f0
Gon Gis <g0ng0n.dev@gmail.com>
2020-11-21 17:53:20 +0100

    adding the call to the SetupDatabase function

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"github.com/g0ng0n-dev/webservicito/database"
+	"log"
 	"github.com/g0ng0n-dev/webservicito/product"
 	"net/http"
+	_"github.com/go-sql-driver/mysql"
 )
 
 const apiBasePath = "/api"
 func main() {
-
+	database.SetupDatabase()
 	product.SetupRoute(apiBasePath)
 	http.ListenAndServe(":5000", nil)
 }
