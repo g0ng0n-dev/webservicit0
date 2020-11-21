3684ea4c10c8a46b3eaac0e6db7acf6180edfe6d
Gonzalo Gisbert <g0ng0n@Gonzalos-MacBook-Pro.local>
2020-11-21 12:58:48 +0100

    initial commit with 2 endpoints for the webservicito

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/g0ng0n-dev/webservicito
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Product struct {
+	ProductID int `json:"productId"`
+	Manufacturer string `json:"manufacturer"`
+	Sku string `json:"sku"`
+	Upc string `json:"upc"`
+	PricePerUnit string `json:"pricePerUnit"`
+	QuantityOnHand int `json:"quantityOnHand"`
+	ProductName string `json:"productName"`
+}
+var productList []Product
+
+func init() {
+	productsJson := `[
+		{
+			"productId": 1,
+			"manufacturer" : "Johns-Jenkins",
+			"sku" : "p5z343vdS",
+			"upc" : "12412412412",
+			"pricePerUnit" : "487.45",
+			"quantityOnHand" : 9703,
+			"productName": "Sticky Note"
+		}
+	]
+	`
+	err := json.Unmarshal([]byte(productsJson), &productList)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getNextId() int {
+	highestID := 1
+	for _, product := range productList {
+		if highestID < product.ProductID {
+			highestID = product.ProductID
+		}
+	}
+	return highestID + 1
+}
+
+func productsHandler(w http.ResponseWriter, r *http.Request){
+	switch r.Method{
+	case http.MethodGet:
+		productsJson, err := json.Marshal(productList)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(productsJson)
+	case http.MethodPost:
+		// add a new product to the list
+		var newProduct Product
+		bodyBytes, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		err = json.Unmarshal(bodyBytes, &newProduct)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		if newProduct.ProductID != 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		newProduct.ProductID = getNextId()
+		productList = append(productList, newProduct)
+		w.WriteHeader(http.StatusCreated)
+		return
+	}
+}
+func main() {
+	http.HandleFunc("/products", productsHandler)
+	http.ListenAndServe(":5000", nil)
+}
